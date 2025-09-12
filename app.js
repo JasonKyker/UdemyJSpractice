d4d74745e3a23d10879e5e2df2efffd67a861730
JasonKyker <jasonk.dsj@gmail.com>
2025-09-12 10:51:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+if (1 === 1) {
+    console.log("Its True!");
+
+}
+
+let rating = 6
+;
+
+
+if (rating <= 3) {
+    console.log(`you are not a superstar!`);
+}
+
+if (rating > 3) {
+    console.log(`Why are you an over achiever?`);
+}
+
+let num = 37;
+
+if (num % 2 !== 0) {
+    console.log(`odd number!`);
+}
